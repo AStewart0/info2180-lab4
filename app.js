9dc164e3cbcb5bd80e5bcc739690547f64bd091f
Antoine Stewart <Stewartant12@gmail.com>
2025-11-21 11:36:25 -0500

    Added AJAX call to show superhero list

---
+++ app.js
@@ -0,0 +1,9 @@
+document.getElementById("search-btn").addEventListener("click", () => {
+
+    fetch("superheroes.php")
+        .then(response => response.text())
+        .then(data => {
+            alert(data);
+        })
+        .catch(error => console.error("Error:", error));
+});
